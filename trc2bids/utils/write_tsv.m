0b1e9b4ef6cdcc4d10fa27def2347e503bd4b845
Paul L. Smits <paul.l.smits@outlook.com>
2021-08-26 13:24:22 +0200

    feat(trc2bids): automatically update group permissions to write, so that file group can edit db.
    dir maker, json writer, tsv writer, and trc2brainvision all add file attribute write (+w) to the group users (g).

--- trc2bids/utils/write_tsv.m
+++ trc2bids/utils/write_tsv.m
@@ -4,5 +4,10 @@
 function write_tsv(filename, tsv)
 ft_info('writing %s\n', filename);
 writetable(tsv, filename, 'Delimiter', 'tab', 'FileType', 'text');
-
+try
+    fileattrib(filename,'+w','g')
+    %disp('tsv write permissions in output directory set for group')
+catch
+    warning('Could not automatically set group permissions. /n Check permissions for file %s', filename)
+end
 %%%%%
